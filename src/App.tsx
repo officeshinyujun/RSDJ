db16f6774119d4ae461fa1001f6dbf5be1ca82dd
officeshinyujun <officeshinyujun@gmail.com>
2025-01-02 16:52:24 +0900

    fix/cssfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import MainPage from "./page/mainPage/mainPage.tsx";
 import StartPage from "./page/startPage/startPage.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import EndPage from "./page/endPage/endPage.tsx";
+import styles from "./index.module.scss"
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<StartPage/>}/>
-            <Route path="/ingame" element={<MainPage/>}/>
-            <Route path="/end" element={<EndPage/>}/>
-        </Routes>
-    </BrowserRouter>
+      <div style={styles}>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<StartPage/>}/>
+                  <Route path="/ingame" element={<MainPage/>}/>
+                  <Route path="/end" element={<EndPage/>}/>
+              </Routes>
+          </BrowserRouter>
+      </div>
   )
 }
 
